9ea410efacc22b2033e5db7371b4e8205babc65a
muk-as <muk-as@yandex.ru>
2023-12-16 07:31:42 +0300

    5948 | 63 files | https://t.me/DOTA_DM

--- game/bin/win64/sdk/!GlobalTypes.hpp
+++ game/bin/win64/sdk/!GlobalTypes.hpp
@@ -13652,7 +13652,7 @@ public:
 // Has Trivial Constructor
 // Has Trivial Destructor
 // 
-// MResourceTypeForInfoType "vcompmat@‘øú"
+// MResourceTypeForInfoType "vcompmat@rúú"
 class InfoForResourceTypeCCompositeMaterialKit
 {
 private:
