ebdb58ba17a1e37d34fa58d05cfc5ed6872cf8bc
muk-as <muk-as@yandex.ru>
2024-11-21 02:52:05 +0300

    6262 | 141 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/server/CDOTA_Modifier_StormSpirit_Electric_Rave.hpp
+++ sdk/include/source2sdk/server/CDOTA_Modifier_StormSpirit_Electric_Rave.hpp
@@ -14,17 +14,19 @@ namespace source2sdk::server
     // Registered alignment: unknown
     // Alignment: unknown
     // Standard-layout class: false
-    // Size: 0x1710
+    // Size: 0x1718
     // Has VTable
     #pragma pack(push, 1)
     class CDOTA_Modifier_StormSpirit_Electric_Rave : public client::CDOTA_Buff
     {
     public:
-        int32_t shard_attack_speed_bonus; // 0x1708        
-        [[maybe_unused]] std::uint8_t pad_0x170c[0x4];
+        float overload_aoe; // 0x1708        
+        int32_t overload_damage; // 0x170c        
+        int32_t shard_attack_speed_bonus; // 0x1710        
+        [[maybe_unused]] std::uint8_t pad_0x1714[0x4];
     };
     #pragma pack(pop)
     
     // Cannot assert offsets of fields in CDOTA_Modifier_StormSpirit_Electric_Rave because it is not a standard-layout class
-    static_assert(sizeof(CDOTA_Modifier_StormSpirit_Electric_Rave) == 0x1710);
+    static_assert(sizeof(CDOTA_Modifier_StormSpirit_Electric_Rave) == 0x1718);
 };
