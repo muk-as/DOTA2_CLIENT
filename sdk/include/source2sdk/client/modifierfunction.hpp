52639ea8b8efc99e903041aeb470689a332cc2ac
muk-as <muk-as@yandex.ru>
2024-12-21 05:17:25 +0300

    6283 | 67 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/client/modifierfunction.hpp
+++ sdk/include/source2sdk/client/modifierfunction.hpp
@@ -13,7 +13,7 @@ namespace source2sdk::client
     // Size: 0x4
     enum class modifierfunction : std::uint32_t
     {
-        // MScriptDescription "pQT)˚ GetModifierPreAttack_BonusDamage"
+        // MScriptDescription "‡aT)˚ GetModifierPreAttack_BonusDamage"
         MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE = 0x0,
         // MScriptDescription "GetModifierPreAttack_BonusDamage_Proc GetModifierPreAttack_BonusDamage_Target"
         MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE_TARGET = 0x1,
@@ -47,7 +47,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_PERSISTENT_INVISIBILITY = 0xf,
         // MScriptDescription "GetModifierMoveSpeedOverride GetModifierMoveSpeedBonus_Constant"
         MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT = 0x10,
-        // MScriptDescription " GetModifierMoveSpeedOverride"
+        // MScriptDescription "pcT)˚ GetModifierMoveSpeedOverride"
         MODIFIER_PROPERTY_MOVESPEED_BASE_OVERRIDE = 0x11,
         // MScriptDescription "GetModifierMoveSpeedBonus_Percentage_Unique GetModifierMoveSpeedBonus_Percentage"
         MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE = 0x12,
@@ -73,7 +73,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_MOVESPEED_LIMIT = 0x1c,
         // MScriptDescription "GetModifierFixedAttackRate GetModifierAttackSpeedBaseOverride"
         MODIFIER_PROPERTY_ATTACKSPEED_BASE_OVERRIDE = 0x1d,
-        // MScriptDescription "`TT)˚ GetModifierFixedAttackRate"
+        // MScriptDescription "–dT)˚ GetModifierFixedAttackRate"
         MODIFIER_PROPERTY_FIXED_ATTACK_RATE = 0x1e,
         // MScriptDescription "GetModifierAttackSpeed_Limit GetModifierAttackSpeedBonus_Constant"
         MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT = 0x1f,
@@ -93,7 +93,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_BASE_ATTACK_TIME_PERCENTAGE = 0x26,
         // MScriptDescription "GetModifierBonusDamageOutgoing_Percentage GetModifierAttackPointConstant"
         MODIFIER_PROPERTY_ATTACK_POINT_CONSTANT = 0x27,
-        // MScriptDescription "ÄUT)˚ GetModifierBonusDamageOutgoing_Percentage"
+        // MScriptDescription "eT)˚ GetModifierBonusDamageOutgoing_Percentage"
         MODIFIER_PROPERTY_BONUSDAMAGEOUTGOING_PERCENTAGE = 0x28,
         // MScriptDescription "GetModifierDamageOutgoing_Percentage_Illusion GetModifierDamageOutgoing_Percentage"
         MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE = 0x29,
@@ -247,7 +247,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_ATTACK_RANGE_BONUS_UNIQUE = 0x73,
         // MScriptDescription "GetModifierMaxAttackRange GetModifierAttackRangeBonusPercentage"
         MODIFIER_PROPERTY_ATTACK_RANGE_BONUS_PERCENTAGE = 0x74,
-        // MScriptDescription "–]T)˚ GetModifierMaxAttackRange"
+        // MScriptDescription "@nT)˚ GetModifierMaxAttackRange"
         MODIFIER_PROPERTY_MAX_ATTACK_RANGE = 0x75,
         // MScriptDescription "GetModifierProjectileSpeedBonusPercentage GetModifierProjectileSpeedBonus"
         MODIFIER_PROPERTY_PROJECTILE_SPEED_BONUS = 0x76,
@@ -255,7 +255,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_PROJECTILE_SPEED_BONUS_PERCENTAGE = 0x77,
         // MScriptDescription "ReincarnateTime GetModifierProjectileName"
         MODIFIER_PROPERTY_PROJECTILE_NAME = 0x78,
-        // MScriptDescription "p^T)˚ ReincarnateTime"
+        // MScriptDescription "‡nT)˚ ReincarnateTime"
         MODIFIER_PROPERTY_REINCARNATION = 0x79,
         // MScriptDescription "GetModifierConstantRespawnTime ReincarnateSuppressFX"
         MODIFIER_PROPERTY_REINCARNATION_SUPPRESS_FX = 0x7a,
@@ -431,7 +431,7 @@ namespace source2sdk::client
         MODIFIER_EVENT_ON_DEATH_PREVENTED = 0xcf,
         // MScriptDescription "Unused OnStateChanged"
         MODIFIER_EVENT_ON_STATE_CHANGED = 0xd0,
-        // MScriptDescription "†hT)˚ Unused"
+        // MScriptDescription "yT)˚ Unused"
         MODIFIER_EVENT_ON_ORB_EFFECT = 0xd1,
         // MScriptDescription " OnProcessCleave"
         MODIFIER_EVENT_ON_PROCESS_CLEAVE = 0xd2,
@@ -481,7 +481,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_MODEL_CHANGE = 0xe8,
         // MScriptDescription "GetModifierModelScaleAnimateTime GetModifierModelScale"
         MODIFIER_PROPERTY_MODEL_SCALE = 0xe9,
-        // MScriptDescription "PkT)˚ GetModifierModelScaleAnimateTime"
+        // MScriptDescription "¿{T)˚ GetModifierModelScaleAnimateTime"
         MODIFIER_PROPERTY_MODEL_SCALE_ANIMATE_TIME = 0xea,
         // MScriptDescription "GetModifierModelScaleConstant GetModifierModelScaleUseInOutEase"
         MODIFIER_PROPERTY_MODEL_SCALE_USE_IN_OUT_EASE = 0xeb,
@@ -493,7 +493,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_IS_SHARD = 0xee,
         // MScriptDescription "GetActivityTranslationModifiers GetModifierRadarCooldownReduction"
         MODIFIER_PROPERTY_RADAR_COOLDOWN_REDUCTION = 0xef,
-        // MScriptDescription "kT)˚ GetActivityTranslationModifiers"
+        // MScriptDescription "`|T)˚ GetActivityTranslationModifiers"
         MODIFIER_PROPERTY_TRANSLATE_ACTIVITY_MODIFIERS = 0xf0,
         // MScriptDescription "GetUnitLifetimeFraction GetAttackSound"
         MODIFIER_PROPERTY_TRANSLATE_ATTACK_SOUND = 0xf1,
@@ -517,7 +517,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_OVERRIDE_ABILITY_SPECIAL = 0xfa,
         // MScriptDescription "GetModifierAbilityLayout GetModifierOverrideAbilitySpecialValue"
         MODIFIER_PROPERTY_OVERRIDE_ABILITY_SPECIAL_VALUE = 0xfb,
-        // MScriptDescription "PmT)˚ GetModifierAbilityLayout"
+        // MScriptDescription "¿}T)˚ GetModifierAbilityLayout"
         MODIFIER_PROPERTY_ABILITY_LAYOUT = 0xfc,
         // MScriptDescription "OnDominated"
         MODIFIER_EVENT_ON_DOMINATED = 0xfd,
@@ -531,7 +531,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_PRESERVE_PARTICLES_ON_MODEL_CHANGE = 0x101,
         // MScriptDescription "GetModifierIgnoreCooldown OnAttackFinished"
         MODIFIER_EVENT_ON_ATTACK_FINISHED = 0x102,
-        // MScriptDescription "‡nT)˚ GetModifierIgnoreCooldown"
+        // MScriptDescription "PT)˚ GetModifierIgnoreCooldown"
         MODIFIER_PROPERTY_IGNORE_COOLDOWN = 0x103,
         // MScriptDescription "GetModifierCanAttackTrees"
         MODIFIER_PROPERTY_CAN_ATTACK_TREES = 0x104,
@@ -589,7 +589,7 @@ namespace source2sdk::client
         MODIFIER_EVENT_SPELL_APPLIED_SUCCESSFULLY = 0x11e,
         // MScriptDescription "GetModifierPropetyFailAttack GetModifierAvoidDamageAfterReductions"
         MODIFIER_PROPERTY_AVOID_DAMAGE_AFTER_REDUCTIONS = 0x11f,
-        // MScriptDescription "prT)˚ GetModifierPropetyFailAttack"
+        // MScriptDescription "‡ÇT)˚ GetModifierPropetyFailAttack"
         MODIFIER_PROPERTY_FAIL_ATTACK = 0x120,
         // MScriptDescription "GetModifierSuppressFullscreenDeathFX GetModifierPrereduceIncomingDamage_Mult"
         MODIFIER_PROPERTY_PREREDUCE_INCOMING_DAMAGE_MULT = 0x121,
@@ -611,7 +611,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_PROJECTILE_SPEED = 0x129,
         // MScriptDescription "GetModifierBecomeStrength GetModifierProjectileSpeedTarget"
         MODIFIER_PROPERTY_PROJECTILE_SPEED_TARGET = 0x12a,
-        // MScriptDescription "sT)˚ GetModifierBecomeStrength"
+        // MScriptDescription "`ÑT)˚ GetModifierBecomeStrength"
         MODIFIER_PROPERTY_BECOME_STRENGTH = 0x12b,
         // MScriptDescription "GetModifierBecomeIntelligence GetModifierBecomeAgility"
         MODIFIER_PROPERTY_BECOME_AGILITY = 0x12c,
@@ -623,7 +623,7 @@ namespace source2sdk::client
         MODIFIER_EVENT_ON_FORCE_PROC_MAGIC_STICK = 0x12f,
         // MScriptDescription "GetModifierShareXPRune OnDamageHPLoss"
         MODIFIER_EVENT_ON_DAMAGE_HPLOSS = 0x130,
-        // MScriptDescription "¿tT)˚ GetModifierShareXPRune"
+        // MScriptDescription "0ÖT)˚ GetModifierShareXPRune"
         MODIFIER_PROPERTY_SHARE_XPRUNE = 0x131,
         // MScriptDescription "GetModifierHasBonusNeutralItemChoice GetModifierNoFreeTPScrollOnDeath"
         MODIFIER_PROPERTY_NO_FREE_TP_SCROLL_ON_DEATH = 0x132,
@@ -647,7 +647,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_ABILITY_POINTS = 0x13b,
         // MScriptDescription "GetModifierBuybackPenaltyPercent"
         MODIFIER_PROPERTY_BUYBACK_PENALTY_PERCENT = 0x13c,
-        // MScriptDescription "–uT)˚ GetModifierItemSellbackCost"
+        // MScriptDescription "@ÜT)˚ GetModifierItemSellbackCost"
         MODIFIER_PROPERTY_ITEM_SELLBACK_COST = 0x13d,
         // MScriptDescription "GetModifierFixedManaRegen GetModifierDisassembleAnything"
         MODIFIER_PROPERTY_DISASSEMBLE_ANYTHING = 0x13e,
@@ -705,7 +705,7 @@ namespace source2sdk::client
         MODIFIER_PROPERTY_TIER_TOKEN_REROLL = 0x158,
         // MScriptDescription "GetModifierTotal_ConstantBlockStacking GetVisionDegreeRestriction"
         MODIFIER_PROPERTY_VISION_DEGREES_RESTRICTION = 0x159,
-        // MScriptDescription " yT)˚ GetModifierTotal_ConstantBlockStacking"
+        // MScriptDescription "êâT)˚ GetModifierTotal_ConstantBlockStacking"
         MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK_STACKING = 0x15a,
         // MScriptDescription "OnTierTokenRerolled GetModifierInventorySlotRestricted"
         MODIFIER_PROPERTY_INVENTORY_SLOT_RESTRICTED = 0x15b,
--- sdk/include/source2sdk/server/CDOTA_Modifier_Seasonal_PartyHat.hpp
+++ sdk/include/source2sdk/server/CDOTA_Modifier_Seasonal_PartyHat.hpp
@@ -21,7 +21,7 @@ namespace source2sdk::server
     // Registered alignment: unknown
     // Alignment: unknown
     // Standard-layout class: false
-    // Size: 0x1778
+    // Size: 0x1780
     // Has VTable
     #pragma pack(push, 1)
     class CDOTA_Modifier_Seasonal_PartyHat : public client::CDOTA_Buff
@@ -30,25 +30,25 @@ namespace source2sdk::server
         int32_t controlled_unit_search_radius; // 0x1708        
         bool child_modifier; // 0x170c        
         [[maybe_unused]] std::uint8_t pad_0x170d[0x3]; // 0x170d
-        CUtlString m_strEffectName; // 0x1710        
-        client::ParticleIndex_t m_nHatFXIndex; // 0x1718        
-        [[maybe_unused]] std::uint8_t pad_0x171c[0x4]; // 0x171c
+        // m_vecParticles has a template type with potentially unknown template parameters. You can try uncommenting the field below.
+        // CUtlVector<client::ParticleIndex_t> m_vecParticles;
+        char m_vecParticles[0x18]; // 0x1710        
         // m_vecHatColors has a template type with potentially unknown template parameters. You can try uncommenting the field below.
         // CUtlVector<int32_t> m_vecHatColors;
-        char m_vecHatColors[0x18]; // 0x1720        
-        uint32_t m_unVersion; // 0x1738        
-        [[maybe_unused]] std::uint8_t pad_0x173c[0x4]; // 0x173c
+        char m_vecHatColors[0x18]; // 0x1728        
+        uint32_t m_unVersion; // 0x1740        
+        [[maybe_unused]] std::uint8_t pad_0x1744[0x4]; // 0x1744
         // m_vecNearbyUnits has a template type with potentially unknown template parameters. You can try uncommenting the field below.
         // CUtlVector<CHandle<server::CBaseEntity>> m_vecNearbyUnits;
-        char m_vecNearbyUnits[0x18]; // 0x1740        
+        char m_vecNearbyUnits[0x18]; // 0x1748        
         // m_vecUnitIdleStartTimes has a template type with potentially unknown template parameters. You can try uncommenting the field below.
         // CUtlVector<entity2::GameTime_t> m_vecUnitIdleStartTimes;
-        char m_vecUnitIdleStartTimes[0x18]; // 0x1758        
-        bool m_bPlayEndcapOnNext; // 0x1770        
-        [[maybe_unused]] std::uint8_t pad_0x1771[0x7];
+        char m_vecUnitIdleStartTimes[0x18]; // 0x1760        
+        bool m_bPlayEndcapOnNext; // 0x1778        
+        [[maybe_unused]] std::uint8_t pad_0x1779[0x7];
     };
     #pragma pack(pop)
     
     // Cannot assert offsets of fields in CDOTA_Modifier_Seasonal_PartyHat because it is not a standard-layout class
-    static_assert(sizeof(CDOTA_Modifier_Seasonal_PartyHat) == 0x1778);
+    static_assert(sizeof(CDOTA_Modifier_Seasonal_PartyHat) == 0x1780);
 };
