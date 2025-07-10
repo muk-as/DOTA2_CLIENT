0e5f1b7492d7bfa0110995ba65e9710d8bd764e4
muk-as <muk-as@yandex.ru>
2025-07-11 02:12:48 +0300

    6487 | 138 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/client/DOTALimits_t.hpp
+++ sdk/include/source2sdk/client/DOTALimits_t.hpp
@@ -18,17 +18,17 @@ namespace source2sdk
         // Size: 0x4
         enum class DOTALimits_t : std::uint32_t
         {
-            // MScriptDescription "Max number of players per team. Max number of players connected to the server including spectators."
+            // MScriptDescription "m_hVarmint Max number of players connected to the server including spectators."
             DOTA_MAX_PLAYERS = 0x40,
-            // MScriptDescription "Max number of player teams supported. Max number of players per team."
+            // MScriptDescription "Max number of players per team."
             DOTA_MAX_TEAM = 0x18,
             // MScriptDescription "Max number of non-spectator players supported. Max number of player teams supported."
             DOTA_MAX_PLAYER_TEAMS = 0xa,
-            // MScriptDescription "Max number of non-spectator players supported."
+            // MScriptDescription "How many spectators can watch. Max number of non-spectator players supported."
             DOTA_MAX_TEAM_PLAYERS = 0x18,
             // MScriptDescription "Max number of viewers in a spectator lobby. How many spectators can watch."
             DOTA_MAX_SPECTATOR_TEAM_SIZE = 0x28,
-            // MScriptDescription "Default number of players per team. Max number of viewers in a spectator lobby."
+            // MScriptDescription "Max number of viewers in a spectator lobby."
             DOTA_MAX_SPECTATOR_LOBBY_SIZE = 0xf,
             // MScriptDescription "Default number of non-spectator players supported. Default number of players per team."
             DOTA_DEFAULT_MAX_TEAM = 0x5,
--- sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_InlineNodeSkipSelector.hpp
+++ sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_InlineNodeSkipSelector.hpp
@@ -27,7 +27,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "BaseDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo "XûÂ&û"
+        // static metadata: MPulseCellOutflowHookInfo "Ã&û"
         // static metadata: MPulseFunctionHiddenInTool
         // static metadata: MPulseSelectorAllowRequirementCriteria
         // static metadata: MPulseSelectorAllowRequirementCriteria
