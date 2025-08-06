8810add6ff057081b712e38808bb75a8e1eb29e3
muk-as <muk-as@yandex.ru>
2025-08-06 03:53:49 +0300

    6501 | 143 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_Step_PublicOutput.hpp
+++ sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_Step_PublicOutput.hpp
@@ -25,7 +25,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "BaseDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo "0à2û"
+        // static metadata: MPulseCellOutflowHookInfo "pß2û"
         // static metadata: MPulseCellWithCustomDocNode
         #pragma pack(push, 1)
         class CPulseCell_Step_PublicOutput : public source2sdk::pulse_runtime_lib::CPulseCell_BaseFlow
