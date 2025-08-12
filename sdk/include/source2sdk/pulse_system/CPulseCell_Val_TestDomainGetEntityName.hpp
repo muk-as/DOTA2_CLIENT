ad7b331f11f00d58b7824252ea91616c2af2fc1f
muk-as <muk-as@yandex.ru>
2025-08-12 03:31:04 +0300

    6509 | 186 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/pulse_system/CPulseCell_Val_TestDomainGetEntityName.hpp
+++ sdk/include/source2sdk/pulse_system/CPulseCell_Val_TestDomainGetEntityName.hpp
@@ -24,7 +24,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "TestDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo "pÛˆ)û"
+        // static metadata: MPulseCellOutflowHookInfo "pÛŸ)û"
         // static metadata: MPropertyFriendlyName "Get Fake Entity Name"
         #pragma pack(push, 1)
         class CPulseCell_Val_TestDomainGetEntityName : public source2sdk::pulse_runtime_lib::CPulseCell_BaseValue
