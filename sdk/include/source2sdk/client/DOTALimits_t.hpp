2b75b484a635544f69849eda45b0ec5d6bf9955e
muk-as <muk-as@yandex.ru>
2025-09-05 00:27:28 +0300

    6536 | 183 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/client/DOTALimits_t.hpp
+++ sdk/include/source2sdk/client/DOTALimits_t.hpp
@@ -32,7 +32,7 @@ namespace source2sdk
             DOTA_MAX_SPECTATOR_LOBBY_SIZE = 0xf,
             // MScriptDescription "Default number of non-spectator players supported. Default number of players per team."
             DOTA_DEFAULT_MAX_TEAM = 0x5,
-            // MScriptDescription "‡û1û Default number of non-spectator players supported."
+            // MScriptDescription "—û1û Default number of non-spectator players supported."
             DOTA_DEFAULT_MAX_TEAM_PLAYERS = 0xa,
         };
     };
--- sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_Outflow_IntSwitch.hpp
+++ sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_Outflow_IntSwitch.hpp
@@ -25,7 +25,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "BaseDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo "°â2û"
+        // static metadata: MPulseCellOutflowHookInfo "°ò2û"
         // static metadata: MPulseCellWithCustomDocNode
         #pragma pack(push, 1)
         class CPulseCell_Outflow_IntSwitch : public source2sdk::pulse_runtime_lib::CPulseCell_BaseFlow
