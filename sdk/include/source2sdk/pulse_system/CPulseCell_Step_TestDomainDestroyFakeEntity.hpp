b8e7022ef9da15655200ef97f52703713f445cb7
muk-as <muk-as@yandex.ru>
2025-09-09 01:50:39 +0300

    6543 | 38 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/pulse_system/CPulseCell_Step_TestDomainDestroyFakeEntity.hpp
+++ sdk/include/source2sdk/pulse_system/CPulseCell_Step_TestDomainDestroyFakeEntity.hpp
@@ -24,7 +24,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "TestDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo "HÛÇæû"
+        // static metadata: MPulseCellOutflowHookInfo "HÛÌæû"
         // static metadata: MPropertyFriendlyName "Destroy Fake Entity"
         #pragma pack(push, 1)
         class CPulseCell_Step_TestDomainDestroyFakeEntity : public source2sdk::pulse_runtime_lib::CPulseCell_BaseFlow
