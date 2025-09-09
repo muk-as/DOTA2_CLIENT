5881bcd6d7555303a40442985b78a877e170b56d
muk-as <muk-as@yandex.ru>
2025-09-10 02:35:21 +0300

    6548 | 75 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_InlineNodeSkipSelector.hpp
+++ sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_InlineNodeSkipSelector.hpp
@@ -27,7 +27,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "BaseDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo "ê∂¯"
+        // static metadata: MPulseCellOutflowHookInfo "êΩ¯"
         // static metadata: MPulseFunctionHiddenInTool
         // static metadata: MPulseSelectorAllowRequirementCriteria
         // static metadata: MPulseSelectorAllowRequirementCriteria
