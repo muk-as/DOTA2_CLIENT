f5e6e43a44a3f0f4fc61a60e5cf415c3afb410b9
muk-as <muk-as@yandex.ru>
2025-10-03 04:15:51 +0300

    6580 | 78 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/client/CPulseCell_LerpCameraSettings.hpp
+++ sdk/include/source2sdk/client/CPulseCell_LerpCameraSettings.hpp
@@ -25,7 +25,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "BaseDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo " ‘้%ý"
+        // static metadata: MPulseCellOutflowHookInfo " ก้%ý"
         #pragma pack(push, 1)
         class CPulseCell_LerpCameraSettings : public source2sdk::pulse_runtime_lib::CPulseCell_BaseLerp
         {
--- sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_Inflow_GraphHook.hpp
+++ sdk/include/source2sdk/pulse_runtime_lib/CPulseCell_Inflow_GraphHook.hpp
@@ -24,7 +24,7 @@ namespace source2sdk
         // static metadata: MGetKV3ClassDefaults
         // static metadata: MCellForDomain "BaseDomain"
         // static metadata: MPulseCellMethodBindings
-        // static metadata: MPulseCellOutflowHookInfo "ÀÉ&ý"
+        // static metadata: MPulseCellOutflowHookInfo "ÀÙ&ý"
         // static metadata: MPulseCellWithCustomDocNode
         #pragma pack(push, 1)
         class CPulseCell_Inflow_GraphHook : public source2sdk::pulse_runtime_lib::CPulseCell_Inflow_BaseEntrypoint
