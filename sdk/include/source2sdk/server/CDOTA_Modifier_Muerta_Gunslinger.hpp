64d5a8e4dd1d0fa14918d4c64976131c35a6e061
muk-as <muk-as@yandex.ru>
2026-01-31 02:16:15 +0300

    6689 | 38 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/server/CDOTA_Modifier_Muerta_Gunslinger.hpp
+++ sdk/include/source2sdk/server/CDOTA_Modifier_Muerta_Gunslinger.hpp
@@ -37,6 +37,7 @@ namespace source2sdk
             // m_hSecondaryTarget has a template type with potentially unknown template parameters. You can try uncommenting the field below.
             // CHandle<source2sdk::server::CBaseEntity> m_hSecondaryTarget;
             char m_hSecondaryTarget[0x_]; // 0x_            
+            bool m_bApplyBonusDamage; // 0x_            
             uint8_t _pad_[0x_];
         };
         #pragma pack(pop)
