870d49ea55eb87842c16e778f5fb8faa8571b304
muk-as <muk-as@yandex.ru>
2026-03-27 02:30:32 +0300

    6737 | 32 files | https://t.me/DOTA_DM

--- sdk/include/source2sdk/client/DOTALimits_t.hpp
+++ sdk/include/source2sdk/client/DOTALimits_t.hpp
@@ -20,19 +20,19 @@ namespace source2sdk
         {
             // MScriptDescription "m_hVarmint Max number of players connected to the server including spectators."
             DOTA_MAX_PLAYERS = 0x40,
-            // MScriptDescription "Max number of player teams supported. Max number of players per team."
+            // MScriptDescription "Max number of players per team."
             DOTA_MAX_TEAM = 0x18,
-            // MScriptDescription "Max number of player teams supported."
+            // MScriptDescription "Max number of non-spectator players supported. Max number of player teams supported."
             DOTA_MAX_PLAYER_TEAMS = 0xa,
             // MScriptDescription "How many spectators can watch. Max number of non-spectator players supported."
             DOTA_MAX_TEAM_PLAYERS = 0x18,
             // MScriptDescription "Max number of viewers in a spectator lobby. How many spectators can watch."
             DOTA_MAX_SPECTATOR_TEAM_SIZE = 0x28,
-            // MScriptDescription "Default number of players per team. Max number of viewers in a spectator lobby."
+            // MScriptDescription "Max number of viewers in a spectator lobby."
             DOTA_MAX_SPECTATOR_LOBBY_SIZE = 0xf,
-            // MScriptDescription " Default number of players per team."
+            // MScriptDescription "Default number of non-spectator players supported. Default number of players per team."
             DOTA_DEFAULT_MAX_TEAM = 0x5,
-            // MScriptDescription "Default number of non-spectator players supported."
+            // MScriptDescription "P Default number of non-spectator players supported."
             DOTA_DEFAULT_MAX_TEAM_PLAYERS = 0xa,
         };
     };
